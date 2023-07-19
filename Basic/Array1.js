0b1c3a36ae40bd0eee7488adc15a7039b9d0a4e7
SurajSalunkhe7 <surajsalunkhe7768@gmail.com>
2023-07-19 20:39:36 +0530

    Array

---
+++ Basic/Array1.js
@@ -0,0 +1,9 @@
+// int Arr[5] = {10,20,30,40,50};   // c,c++,java
+// Arr is one dimentional array which contains 5 elements each element is of type number
+var Arr = [10, 20, 30, 40, 50];
+console.log("Length of Array is : " + Arr.length);
+console.log("First element of array is : " + Arr[0]);
+console.log("Second element of array is : " + Arr[1]);
+for (var iCnt = 0; iCnt < Arr.length; iCnt++) {
+    console.log(iCnt + 1 + " Element is : " + Arr[iCnt]);
+}
