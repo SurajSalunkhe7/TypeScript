4b8395ab31bbc275a03140c11dc3a688ed54cfdc
SurajSalunkhe7 <surajsalunkhe7768@gmail.com>
2024-09-24 10:12:24 +0530

    Q.2

---
+++ Assignment3/2.js
@@ -0,0 +1,16 @@
+var Circle = /** @class */ (function () {
+    function Circle(No1, No2) {
+        this.Redius = No1;
+        this.Pi = No2;
+    }
+    Circle.prototype.AreaOfCircle = function () {
+        var Ans = 0;
+        Ans = this.Redius * this.Redius * this.Pi;
+        return Ans;
+    };
+    return Circle;
+}());
+var Ret = 0;
+var Obj = new Circle(7, 3.14);
+Ret = Obj.AreaOfCircle();
+console.log("Area Of Circle is : ", Ret);
