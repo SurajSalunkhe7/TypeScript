f1ed470a3d78fb8eaf4e0fe07b3a099b91000c4b
SurajSalunkhe7 <surajsalunkhe7768@gmail.com>
2024-11-18 17:25:51 +0530

    Basic

---
+++ Basic/12.js
@@ -0,0 +1,5 @@
+// Tuple Demo
+var TupleX = [10, 'Suraj', true, 11.201];
+for (var iCnt = 0; iCnt < TupleX.length; iCnt++) {
+    console.log(TupleX[iCnt]);
+}
---
+++ Basic/12.ts
@@ -0,0 +1,8 @@
+// Tuple Demo
+
+var TupleX:any[] = [10,'Suraj',true,11.20];
+
+for(var iCnt:number = 0; iCnt < TupleX.length; iCnt++)
+    {
+        console.log(TupleX[iCnt]);
+    }
