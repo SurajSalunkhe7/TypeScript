f862772872b02cd562815ed864031c508c0d8e55
SurajSalunkhe7 <surajsalunkhe7768@gmail.com>
2024-11-18 17:26:17 +0530

    Basic

---
+++ Basic/13.js
@@ -0,0 +1,12 @@
+// Regular Function
+function RegularX(No1, No2, No3) {
+    var Addition = 0;
+    Addition = No1 + No2 + No3;
+    return Addition;
+}
+var Value1 = 10;
+var Value2 = 11;
+var Value3 = 12;
+var Ret = 0;
+Ret = RegularX(Value1, Value2, Value3);
+console.log("Addition is : ", Ret);
