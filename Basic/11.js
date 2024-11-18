37b85d2d48360ed2a3aaa5557c018a4259edef83
SurajSalunkhe7 <surajsalunkhe7768@gmail.com>
2024-11-18 17:25:03 +0530

    Basic

---
+++ Basic/11.js
@@ -0,0 +1,5 @@
+// Array Demo
+var Arr = [10, 20, 30, 40, 50];
+for (var iCnt = 0; iCnt < Arr.length; iCnt++) {
+    console.log(Arr[iCnt]);
+}
---
+++ Basic/11.ts
@@ -0,0 +1,8 @@
+// Array Demo
+
+var Arr:number[] = [10,20,30,40,50];
+
+for(var iCnt:number = 0; iCnt < Arr.length; iCnt++)
+    {
+        console.log(Arr[iCnt]);
+    }
